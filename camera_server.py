e6b6a18d999a706e3cf36dbf23f1de7a5b1fee5e
calebclothier <calebclothier@Calebs-MBP.home>
2017-05-17 15:42:47 -0400

    Server side touch ups

--- camera_server.py
+++ camera_server.py
@@ -4,30 +4,40 @@ import socket
 import cv2
 import numpy as np
 
-def start_server():
-	server_socket = socket.socket()
-	server_socket.bind(('0.0.0.0', 8000))
-	server_socket.listen(0)
+class CameraServer():
 
-def stream():
-	# Accept a single connection and make a file-like object out of it
-	connection = server_socket.accept()[0].makefile('rb')
-	try:
-		while True:
-			# Read the length of the image as a 32-bit unsigned int. If the
-			# length is zero, quit the loop
-			image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-			if not image_len:
-				break
-			# Construct a stream to hold the image data and read the image
-			# data from the connection
-			image_stream = io.BytesIO()
-			image_stream.write(connection.read(image_len))
-			# Rewind the stream, open it as an image with PIL and do some
-			# processing on it
-			image_stream.seek(0)
-			image = cv2.imread(image_stream, cv2.IMREAD_COLOR)
-			cv2.imshow(image)
-	finally:
-		connection.close()
-		server_socket.close()
+	def start_server(self):
+		self.server_socket = socket.socket()
+		self.server_socket.bind(('0.0.0.0', 8000))
+		self.server_socket.listen(0)
+
+	def stream(self):
+		# Accept a single connection and make a file-like object out of it
+		connection = self.server_socket.accept()[0].makefile('rb')
+		try:
+			while True:
+				# Read the length of the image as a 32-bit unsigned int. If the
+				# length is zero, quit the loop
+				image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+				if not image_len:
+					break
+				# Construct a stream to hold the image data and read the image
+				# data from the connection
+				image_stream = io.BytesIO()
+				image_stream.write(connection.read(image_len))
+				# Rewind the stream, open it as an image with OpenCV, convert to RGB
+				image_stream.seek(0)
+				data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
+				self.image = cv2.imdecode(data, 1)
+				self.image = self.image[:, :, ::-1]
+				cv2.imshow("Frame", self.image)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+		finally:
+			cv2.destroyAllWindows()
+			connection.close()
+			self.server_socket.close()
+
+camera_server = CameraServer()
+camera_server.start_server()
+camera_server.stream()
